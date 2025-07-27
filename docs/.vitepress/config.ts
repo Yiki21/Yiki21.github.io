9daffc2452b0de0764c7b56bc996b2a93cf3b526
Yiki <zy1772696711@gmail.com>
2025-07-27 13:33:11 +0800

    Add the Favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,6 +27,10 @@ export default defineConfig({
             'link',
             { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" },
         ],
+        [
+            'link',
+            { rel : 'icon', href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗿</text></svg>" }
+        ],
     ],
     description: 'A blog about web development, programming, and what I learn.',
     srcDir: './blogs',
